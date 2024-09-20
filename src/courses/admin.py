2afdbe1efc7fa975eb0d1174d2bb41356bc1f1f8
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-09-20 10:42:51 +0300

    created lesson model

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course,Lesson
 from django.utils.html import format_html
 from cloudinary import CloudinaryImage
 # Register your models here.
-
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    readonly_fields = ['updated',]
+    extra = 0
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    inlines= [LessonInline]
     list_display =['title','status','access']
     list_filter = ['status','access']
     fields = ['title','description','status','image','access','display_image']
     readonly_fields = ['display_image']
     def display_image(self,obj,*args,**kwargs):
-        image_url = obj.image.url
+        image_url = obj.image_admin
         cloudinary_id = str(obj.image)
         cloudinary_html = CloudinaryImage(cloudinary_id).image(width=500)
         cloudinary_html2 = obj.image.image(width=200) # another way to do it 
         
 
-        return format_html(cloudinary_html)
+        return format_html(f"<img src={image_url} />" )
     
     display_image.short_description = 'current image'
--- src/courses/models.py
+++ src/courses/models.py
@@ -36,6 +36,70 @@ class Course(models.Model):
     @property
     def is_published(self):
         return self.status == 'pub'
+    @property
+    def image_admin(self):
+        if not self.image:
+            return ""
+        image_options ={
+            "width":200
+        }
+        url = self.image.build_url(**image_options) # unpacking te image dict
+
+        return url
+    def get_image_detail(self,as_html=False,width=750):
+        if not self.image:
+            return ""
+        image_options ={
+            "width":width
+        }
+        if as_html:
+            return self.image.image(**image_options)
+        url = self.image.build_url(**image_options) # unpacking te image dict
+
+        return url
+        def get_image_thumbnail(self,as_html=False,width=500):
+            if not self.image:
+                return ""
+            image_options ={
+                "width":width
+            }
+            if as_html:
+                return self.image.image(**image_options)
+            url = self.image.build_url(**image_options) # unpacking te image dict
+
+            return url
+
     
+"""
+
+ in python the @property decorator is used to 
+define attributes taht are computed dynamically 
+rather than stored as fixed values 
+it provides a way to encapsulate teh logic for calculating the attributes 
+value, making the cass interface more readable and mantanable 
+SETTER METHOD : you can optinally define a setter method using the 
+@property.setter decorator . this allows you to mdofy the attributes value 
+potentailly performing validation or calculations before updateing the underlying 
+data 
+DELETER METHOD : simlilarly you can define a deleter method using the 
+@property.deleter decorator to remote the attriubtes 
+
+ """
 
 
+class Lesson(models.Model):
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    description = models.TextField(blank=True,null=True)
+    thumbnail = CloudinaryField('image',null=True,blank=True)
+    video = CloudinaryField('video',resource_type='video',null=True,blank=True)
+    can_preview = models.BooleanField(default=False,help_text="if user dont have access to this course can they see this ")
+    order = models.IntegerField(default=0)
+    status = models.CharField(
+        max_length=10,
+        choices=PublishStatus.choices,
+        default= PublishStatus.PUBLISHED
+    )
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering =['order','-updated']
